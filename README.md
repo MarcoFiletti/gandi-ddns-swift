f38d08a8a17e0a2297e7cdcc61d843a53fa6004c
Marco Filetti <marco.filetti@hiit.fi>
2018-05-15 12:58:08 +0200

    basic xcodeproj

---
+++ README.md
@@ -0,0 +1,3 @@
+# gandi-ddns-swift
+
+A description of this package.
---
+++ Sources/gandi-ddns-swift/test.swift
@@ -0,0 +1,16 @@
+//
+//  test.swift
+//  gandi-ddns-swift
+//
+//  Created by Marco Filetti on 15/05/2018.
+//
+
+import Foundation
+
+class Test {
+    
+    static func quaz() -> String {
+        return "vaff"
+    }
+    
+}
