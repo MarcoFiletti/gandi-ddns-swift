5fce252636d45c42e042c0b7ae2a27fd3858a672
Marco Filetti <marco.filetti@hiit.fi>
2018-05-26 22:38:19 +0300

    reading from config json or asking for config data

--- Sources/GandiDDNS/main.swift
+++ Sources/GandiDDNS/main.swift
@@ -1,39 +1,39 @@
 import Foundation
 import GandiDDNSLib
 
-func readGandiKey() {
-    let keyname = "GandiAPI.key"
-    let url: URL = URL(fileURLWithPath: keyname)
-    if let data = try? Data(contentsOf: url),
-       let key = String(data: data, encoding: .utf8) {
-        print("Key is: \"\(key)\"")
-    } else {
-        print("Key in key: >", terminator: "")
-        if let input = readLine(), let data = input.data(using: .utf8) {
-            do {
-                try data.write(to: url)
-                print("Saved key")
-            } catch {
-                print("Failed to save key")
-            }
-        }
+/// Asks config from user and saves it, then quits
+func askForConfigAndQuit() {
+    print("Please input details")
+    print("Domain name: >", terminator: "")
+    guard let domainData = readLine()?.data(using: .utf8) else {
+        print("Failed")
+        exit(1)
     }
+    print("API Key: >", terminator: "")
+    guard let keyData = readLine()?.data(using: .utf8) else {
+        print("Failed")
+        exit(1)
+    }
+    guard let domainName = String(data: domainData, encoding: .utf8),
+          let key = String(data: keyData, encoding: .utf8) else {
+            print("Failed to convert data")
+            exit(1)
+    }
+
+    try! ConfigReader.saveConfig(withDomain: domainName, key: key)
+    print("Data saved, quitting")
+    exit(0)
 }
 
-func testGandi(dry_run: Bool) {
-
-    if dry_run == false {
-        print("better do a dry run for now")
+/// Reads config from file, or asks user for it and saves it, then quits
+func readConfigOrQuit() -> Config {
+    let maybeConfig = try! ConfigReader.read()
+    if let config = maybeConfig {
+        return config
+    } else {
+        askForConfigAndQuit()
+        fatalError("We should never get here")
     }
-
-    let config = ConfigReader.default
-    Gandi.apply(config: config, dry_run: dry_run)
-// let g1 = try! GandiWrapper(domain: d1)
-// try! g1.getIp(subdomain: "www", type: .A)
-// try! g1.getIp(subdomain: "www", type: .AAAA)
-// try! g1.getIp(subdomain: "vaff", type: .A)
-//  g1.updateIp(subdomain: "testplay", type: .A, newIp: "85.216.202.141")
-
 }
 
 var dry_run = false
@@ -59,37 +59,9 @@ if CommandLine.arguments.count > 1 {
     }
 }
 
-testGandi(dry_run: dry_run)
-
-// readGandiKey()
-func testRead() {
-    if CommandLine.arguments.count == 2 {
-        let fname = CommandLine.arguments[1]
-        
-        let url: URL = URL(fileURLWithPath: fname)
-
-        guard let data = try? Data(contentsOf: url) else {
-            print("Could read data from '\(fname)'")
-            exit(2)
-        }
-        let dec = JSONDecoder()
-        guard let ajson = try? dec.decode(Ajson.self, from: data) else {
-            print("Failed to convert data in '\(fname)'")
-            exit(2)
-        }
-        print("A is \(ajson.a)")
-        
-        if let IPv4String = IPFetcher.getIPv4() {
-            print("IPv4 is: '\(IPv4String)'")
-        } else {
-            print("Failed to get IP")
-        }
-        
-        if let IPv6String = IPFetcher.getIPv6() {
-            print("IPv6 is: '\(IPv6String)'")
-        }
-        
-    } else {
-        print("No args, saving data: \(Test.sav())")
-    }
+if dry_run == false {
+    fatalError("better do a dry run for now")
 }
+
+let config = readConfigOrQuit()
+Gandi.apply(config: config, dry_run: dry_run)
